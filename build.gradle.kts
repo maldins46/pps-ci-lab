3e132c83f6a7d068f80515973ee5f7d0448b1b5c
Lippo <filippo.nardini@studio.unibo.it>
2020-05-04 15:57:06 +0200

    Add scala support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.3.72"
+    scala
 }
 
 repositories {
@@ -8,4 +9,5 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.scala-lang:scala-library:2.11.12")
 }
