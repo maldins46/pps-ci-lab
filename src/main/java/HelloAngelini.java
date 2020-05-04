0d9a6d765e2ce555cb7f9e26fe1a9b471f07c966
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-05-04 15:11:10 +0200

    added println 'Maldini stonks'

--- src/main/java/HelloAngelini.java
+++ src/main/java/HelloAngelini.java
@@ -1,5 +1,9 @@
 public class HelloAngelini{
 
+    public static void main(String[] args) {
+        System.out.println("Maldini stonks");
+    }
+
     @Override
     public String toString() {
         return super.toString();
