7c2f4a45510488f42cf53c5d0db405806a2994ad
riccardomaldini <riccardo.maldini@gmail.com>
2020-05-04 16:26:30 +0200

    added jUnit5 and 4 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.scala-lang:scala-library:2.11.12")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testCompileOnly("junit:junit:4.12")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.3.1")
 }
