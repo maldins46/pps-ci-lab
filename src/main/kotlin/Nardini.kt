8f51bcaf7c52e69b617213c676cb5a4d59f3ec92
Lippo <filippo.nardini@studio.unibo.it>
2020-05-04 15:56:01 +0200

    Improve Nardini to use more workers

--- src/main/kotlin/Nardini.kt
+++ src/main/kotlin/Nardini.kt
@@ -1,4 +1,12 @@
+package pps
+
+import java.util.Set;
+import pps.worker.Worker
 /**
- * Nardini is an oppressor which exploits a slave to pursue his interests.
+ * Nardini is an oppressor which exploits his slaves to pursue his interest.
  */
-class Nardini(val slave: Worker): Worker by slave
+class Nardini(val slave: Set<Worker>): Worker {
+    override fun work() {
+        slave.forEach { it.work() }
+    }
+}
