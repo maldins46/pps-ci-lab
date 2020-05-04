e241814653f7dade009e725b9b9accb3770ae1d1
Francesco Gorini <francocecco97@gmail.com>
2020-05-04 15:12:07 +0200

    Modified in base a your ideas

---
+++ src/main/java/Hello2.java
@@ -0,0 +1,5 @@
+public class Hello2{
+    public static void main(){
+        System.out.println("Hello 2");
+    }
+}
--- src/main/java/HelloGorini.java
+++ src/main/java/HelloGorini.java
@@ -1,3 +1,5 @@
 public class HelloGorini{
-    public static void main(){}
+    public static void main(){
+        System.out.println("Hello world");
+    }
 }
